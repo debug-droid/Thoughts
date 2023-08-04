10757eb98109a02d91176df845cce59b6ce5ef3f
Diego <DiegoprogBr@hotmail.com>
2023-08-04 11:29:49 -0300

    últimas correções

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -1,140 +1,132 @@
-const Tought = require('../models/Thought');
-const User = require('../models/User');
+const Tought = require('../models/Thought')
+const User = require('../models/User')
 
-const { Op } = require('sequelize');
-module.exports = class ToughtController {
-    static async showToughts(req, res) {
+const { Op } = require('sequelize')
 
-        let search = '';
+module.exports = class ToughController {
+  static async dashboard(req, res) {
+    const userId = req.session.userid
 
-        if(req.query.search) {
-            search = req.query.search;
-        }
+    const user = await User.findOne({
+      where: {
+        id: userId,
+      },
+      include: Tought,
+      plain: true,
+    })
 
-        let order = 'DESC';
+    const toughts = user.Thoughts.map((result) => result.dataValues)
 
-        if (req.query.order === 'old') {
-            order = 'ASC';
-        } else {
-            order = 'DESC';
-        }
+    let emptyToughts = true
 
-        const toughtsData = Tought.findAll({
-            include: User,
-            where: {
-                title: {[Op.like]: `%${search}%`},
-            },
-            order: [['createdAt', order]],
-        });
+    if (toughts.length > 0) {
+      emptyToughts = false
+    }
 
-        const toughts = toughtsData.Toughts.map((result) => result.get({ plain: true }));
+    console.log(toughts)
+    console.log(emptyToughts)
 
-        let toughtsQty = toughts.length;
+    res.render('toughts/dashboard', { toughts, emptyToughts })
+  }
+
+  static createTought(req, res) {
+    res.render('toughts/create')
+  }
+
+  static createToughtSave(req, res) {
+    const tought = {
+      title: req.body.title,
+      UserId: req.session.userid,
+    }
+
+    Tought.create(tought)
+      .then(() => {
+        req.flash('message', 'Pensamento criado com sucesso!')
+        req.session.save(() => {
+          res.redirect('/toughts/dashboard')
+        })
+      })
+      .catch((err) => console.log())
+  }
+
+  static showToughts(req, res) {
+    console.log(req.query)
+
+    // check if user is searching
+    let search = ''
+
+    if (req.query.search) {
+      search = req.query.search
+    }
+
+    // order results, newest first
+    let order = 'DESC'
+
+    if (req.query.order === 'old') {
+      order = 'ASC'
+    } else {
+      order = 'DESC'
+    }
+
+    Tought.findAll({
+      include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` },
+      },
+      order: [['createdAt', order]],
+    })
+      .then((data) => {
+        let toughtsQty = data.length
 
         if (toughtsQty === 0) {
-            toughtsQty = false;
+          toughtsQty = false
         }
 
-        res.render('toughts/home', { toughts, search, toughtsQty });
-    };
+        const toughts = data.map((result) => result.get({ plain: true }))
 
-    static async dashboard(req, res) {
-        const userId = req.session.userid;
+        res.render('toughts/home', { toughts, toughtsQty, search })
+      })
+      .catch((err) => console.log(err))
+  }
 
-        const user = await User.findOne({
-            where: {
-              id: userId,
-            },
-            include: Tought,
-            plain: true,
-          });
+  static removeTought(req, res) {
+    const id = req.body.id
 
-        // check if user exists
-        if(!user) {
-            res.redirect('/login');
-        }
+    Tought.destroy({ where: { id: id } })
+      .then(() => {
+        req.flash('message', 'Pensamento removido com sucesso!')
+        req.session.save(() => {
+          res.redirect('/toughts/dashboard')
+        })
+      })
+      .catch((err) => console.log())
+  }
 
-        const toughts = user.Toughts.map((result) => result.dataValues);
+  static updateTought(req, res) {
+    const id = req.params.id
 
-        let emptyToughts = false;
+    Tought.findOne({ where: { id: id }, raw: true })
+      .then((tought) => {
+        res.render('toughts/edit', { tought })
+      })
+      .catch((err) => console.log())
+  }
 
-        if(toughts.length === 0) {
-            emptyToughts = true;
-        }
+  static updateToughtPost(req, res) {
+    const id = req.body.id
 
-        res.render('toughts/dashboard', { toughts, emptyToughts });
-    };
-
-    static createTought(req, res) {
-        res.render('toughts/create');
-    };
-
-    static async createToughtSave(req, res) {
-
-        const tought = {
-            title: req.body.title,
-            UserId: req.session.userid
-        };
-
-        try {
-            await Tought.create(tought);
-
-            req.flash('message', 'Pensamento criado com sucesso!');
-
-            req.session.save(() => {
-            res.redirect('/toughts/dashboard');
-        });
-        } catch (error) {
-            console.log(`Aconteceu um erro: ${error}`);
-        }
-
-    };
-
-    static async removeTought(req, res) {
-
-        const id = req.body.id;
-        const UserId = req.session.userid;
-
-        try {
-
-            await Tought.destroy({where: {id: id, UserId: UserId}});
-
-            req.flash('message', 'Pensamento removido com sucesso!');
-
-            req.session.save(() => {
-                res.redirect('/toughts/dashboard');
-            });
-
-        } catch(error) {
-            console.log(`Aconteceu um erro: ${error}`);
-        }
+    const tought = {
+      title: req.body.title,
+      description: req.body.description,
     }
 
-    static async updateTought(req, res) {
-        const id = req.params.id;
-
-        const tought = await Tought.findOne({ where: {id: id }, raw: true });
-
-        res.render('toughts/edit', { tought });
-    }
-
-    static async updateToughtSave(req, res) {
-
-        const id = req.body.id;
-
-        const tought = {
-            title: req.body.title
-        }
-
-        try {
-            await Tought.update(tought, { where: { id: id} });
-            req.flash('message', 'Pensamento atualizado com sucesso!');
-
-            req.session.save(() => {
-                res.redirect('/toughts/dashboard');
-            });
-        } catch(error) {
-            console.log(`Aconteceu um erro: ${error}`);
-        }
-    }
-};
+    Tought.update(tought, { where: { id: id } })
+      .then(() => {
+        req.flash('message', 'Pensamento atualizado com sucesso!')
+        req.session.save(() => {
+          res.redirect('/toughts/dashboard')
+        })
+      })
+      .catch((err) => console.log())
+  }
+}
--- index.js
+++ index.js
@@ -1,82 +1,77 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
-const flash = require('express-flash');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
 
 const app = express();
 
-const conn = require('./db/conn');
+const conn = require("./db/conn");
 
-// models
-const Thought = require('./models/Thought');
-const User = require('./models/User');
+// Models
+const Tought = require("./models/Thought");
 
-// Import Routes
-const toughtsRoutes = require('./routes/toughtsRoutes');
-const authRoutes = require('./routes/authRoutes');
+// routes
+const toughtsRoutes = require("./routes/toughtsRoutes");
+const authRoutes = require("./routes/authRoutes");
+const ToughController = require("./controllers/ToughtController");
 
-// import Controller
-const ToughtController = require('./controllers/ToughtController');
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
-
-// receber resposta do body
 app.use(
-    express.urlencoded({
-        extended: true
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 
 app.use(express.json());
 
-// session midleware
+//session middleware
 app.use(
-    session({
-        name: "session",
-        secret: "nosso_secret",
-        resave: false,
-        saveUninitialized: false,
-        store: new FileStore({
-            logFn: function() {},
-            path: require('path').join(require('os').tmpdir(), 'sessions'),
-        }),
-        cookie: {
-            secure: false,
-            maxAge: 360000,
-            expires: new Date(Date.now() + 360000),
-            httpOnly: true
-        }
+  session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
     }),
-);
+    cookie: {
+      secure: false,
+      maxAge: 3600000,
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    },
+  }),
+)
 
 // flash messages
 app.use(flash());
 
-// public path
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// config session para respostas
+// set session to res
 app.use((req, res, next) => {
+  // console.log(req.session)
+  console.log(req.session.userid);
 
-    if(req.session.userid) {
-        res.locals.session = req.session;
-    }
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
 
-    next();
+  next();
 });
 
-// Routes
-app.use('/toughts', toughtsRoutes);
-app.use('/', authRoutes);
+app.use("/toughts", toughtsRoutes);
+app.use("/", authRoutes);
 
-app.get('/', ToughtController.showToughts);
+app.get("/", ToughController.showToughts);
 
 conn
-    //.sync({ force: true})
-    .sync()
-    .then(() => {
-        app.listen(3000);
-    })
-    .catch((err) => console.log(err));
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
